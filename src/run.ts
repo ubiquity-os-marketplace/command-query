8d60fea5360f0f46623d187c2c5d38258f9e1874
gentlementlegen <fernand.veyrier@epitech.eu>
2024-05-22 12:23:08 +0900

    chore: added error posting

--- src/run.ts
+++ src/run.ts
@@ -1,4 +1,4 @@
-import { CommanderError } from "@commander-js/extra-typings";
+import { CommanderError } from "commander";
 import { Octokit } from "@octokit/rest";
 import { CommandParser } from "./handlers/command-parser";
 import { Context } from "./types/context";
@@ -13,16 +13,22 @@ export async function run(inputs: PluginInputs, env: Env) {
   const args = inputs.eventPayload.comment.body.trim().split(/\s+/);
   const octokit = new Octokit({ auth: env.GITHUB_TOKEN });
   const context = { octokit, payload: inputs.eventPayload } as Context;
+  const commandParser = new CommandParser(context);
   try {
-    const commandParser = new CommandParser(context);
     await commandParser.parse(args);
   } catch (e) {
+    console.log("error", e);
     if (e instanceof CommanderError) {
       if (e.code !== "commander.unknownCommand") {
-        // post error
         console.error(e);
+        console.log("posting!");
         await octokit.issues.createComment({
-          body: `${e}`,
+          body: `\`\`\`
+Failed to run command-query-user.
+${e}
+
+${commandParser.helpInformation()}
+\`\`\``,
           owner: context.payload.repository.owner.login,
           repo: context.payload.repository.name,
           issue_number: context.payload.issue.number,
