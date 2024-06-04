210d51876b71855fcc0ce9243abee35f1060f6fd
whilefoo <whilefoo@users.noreply.github.com>
2024-06-04 23:19:17 +0200

    feat: signature check

--- src/types/env.ts
+++ src/types/env.ts
@@ -3,6 +3,7 @@ import { Type as T, StaticDecode } from "@sinclair/typebox";
 export const envSchema = T.Object({
   SUPABASE_URL: T.String(),
   SUPABASE_KEY: T.String(),
+  UBIQUIBOT_PUBLIC_KEY: T.String(),
 });
 
 export type Env = StaticDecode<typeof envSchema>;
