7daf4c0e123a6d7b6a7ce1e0ee3b531c489e0b84
Mentlegen <9807008+gentlementlegen@users.noreply.github.com>
2024-10-14 16:35:02 +0900

    refactor: update wrangler config and package versions
    Updated wrangler.toml configuration, package scripts, and dependency versions.

--- src/worker.ts
+++ src/worker.ts
@@ -8,10 +8,14 @@ import { Database } from "./types/database";
 import { Env } from "./types/env";
 import { PluginSettings } from "./types/plugin-input";
 
-export default createPlugin<PluginSettings, Env, SupportedEvents>((context) => {
-  const supabase = createClient<Database>(context.env.SUPABASE_URL, context.env.SUPABASE_KEY);
-  return run({ ...context, adapters: createAdapters(supabase, context) });
-}, manifest).then((server) => {
+export default createPlugin<PluginSettings, Env, SupportedEvents>(
+  (context) => {
+    const supabase = createClient<Database>(context.env.SUPABASE_URL, context.env.SUPABASE_KEY);
+    return run({ ...context, adapters: createAdapters(supabase, context) });
+  },
+  // @ts-expect-error strings cannot be assigned to events
+  manifest
+).then((server) => {
   console.log("Plugin initialized.");
   return server;
 });
