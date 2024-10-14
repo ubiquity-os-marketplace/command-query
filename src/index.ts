12aeec24f478128a8c6967493fff0790cf7f1570
Mentlegen <9807008+gentlementlegen@users.noreply.github.com>
2024-10-14 16:50:42 +0900

    refactor: update entry point to src/index.ts
    Renamed src/worker.ts to src/index.ts and updated relevant references.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import { createPlugin } from "@ubiquity-os/ubiquity-os-kernel";
+import { ExecutionContext } from "hono";
 import manifest from "../manifest.json";
 import { createAdapters } from "./adapters";
 import { run } from "./run";
@@ -8,13 +9,17 @@ import { Database } from "./types/database";
 import { Env } from "./types/env";
 import { PluginSettings } from "./types/plugin-input";
 
-export default createPlugin<PluginSettings, Env, SupportedEvents>(
+const pluginPromise = createPlugin<PluginSettings, Env, SupportedEvents>(
   (context) => {
     const supabase = createClient<Database>(context.env.SUPABASE_URL, context.env.SUPABASE_KEY);
     return run({ ...context, adapters: createAdapters(supabase, context) });
   },
   // @ts-expect-error strings cannot be assigned to events
   manifest
-).then((app) => ({
-  fetch: app.fetch,
-}));
+);
+
+export default {
+  async fetch(request: Request, env: Env, context: ExecutionContext) {
+    return (await pluginPromise).fetch(request, env, context);
+  },
+};
