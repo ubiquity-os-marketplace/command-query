ff58863b16828c89541912a5b3a1f0b4a6758532
gentlementlegen <fernand.veyrier@epitech.eu>
2024-10-16 15:32:31 +0900

    refactor: integrate envSchema into plugin creation
    Added envSchema to ensure environment variables are validated.

--- src/index.ts
+++ src/index.ts
@@ -6,23 +6,23 @@ import { createAdapters } from "./adapters";
 import { run } from "./run";
 import { SupportedEvents } from "./types/context";
 import { Database } from "./types/database";
-import { Env } from "./types/env";
+import { Env, envSchema } from "./types/env";
 import { PluginSettings, pluginSettingsSchema } from "./types/plugin-input";
-
-function createPluginWithEnv(env: Record<string, string>) {
-  return createPlugin<PluginSettings, Env, SupportedEvents>(
-    (context) => {
-      const supabase = createClient<Database>(env.SUPABASE_URL, env.SUPABASE_KEY);
-      return run({ ...context, adapters: createAdapters(supabase, context) });
-    },
-    // @ts-expect-error strings cannot be assigned to events
-    manifest,
-    { kernelPublicKey: env.KERNEL_PUBLIC_KEY, settingsSchema: pluginSettingsSchema }
-  );
-}
 
 export default {
   async fetch(request: Request, env: Env, context: ExecutionContext) {
+    function createPluginWithEnv(environment: Env) {
+      return createPlugin<PluginSettings, Env, SupportedEvents>(
+        (context) => {
+          const supabase = createClient<Database>(environment.SUPABASE_URL, environment.SUPABASE_KEY);
+          return run({ ...context, adapters: createAdapters(supabase, context) });
+        },
+        // @ts-expect-error strings cannot be assigned to events
+        manifest,
+        { kernelPublicKey: env.KERNEL_PUBLIC_KEY, settingsSchema: pluginSettingsSchema, envSchema: envSchema }
+      );
+    }
+
     const plugin = await createPluginWithEnv(env);
     return plugin.fetch(request, env, context);
   },
--- src/types/env.ts
+++ src/types/env.ts
@@ -3,6 +3,7 @@ import { StaticDecode, Type as T } from "@sinclair/typebox";
 export const envSchema = T.Object({
   SUPABASE_URL: T.String(),
   SUPABASE_KEY: T.String(),
+  KERNEL_PUBLIC_KEY: T.Optional(T.String()),
 });
 
 export type Env = StaticDecode<typeof envSchema>;
