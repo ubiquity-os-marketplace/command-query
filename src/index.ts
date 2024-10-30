2ff6eb151cfa37acbdad004f634c5f816fd59565
gentlementlegen <fernand.veyrier@epitech.eu>
2024-10-30 14:41:31 +0900

    chore: fixed env

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ import { PluginSettings, pluginSettingsSchema } from "./types/plugin-input";
 export default {
   async fetch(request: Request, env: Env, executionContext: ExecutionContext) {
     console.log(
-      `https://dash.cloudflare.com/${env.CLOUDFLARE_ID}/workers/services/view/${env.CLOUDFLARE_WORKER_NAME}/production/observability/logs?granularity=0&time=%7B"type"%3A"absolute"%2C"to"%3A${Date.now() + 60000}%2C"from"%3A${Date.now()}%7D`
+      `https://dash.cloudflare.com/${env.CLOUDFLARE_ACCOUNT_ID}/workers/services/view/${env.CLOUDFLARE_WORKER_NAME}/production/observability/logs?granularity=0&time=%7B"type"%3A"absolute"%2C"to"%3A${Date.now() + 60000}%2C"from"%3A${Date.now()}%7D`
     );
     return createPlugin<PluginSettings, Env, SupportedEvents>(
       (context) => {
