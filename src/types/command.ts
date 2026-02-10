0c5ee6f4e01e273837c78b5b8aa876d038bd6bac
gentlementlegen <fernand.veyrier@epitech.eu>
2026-02-10 15:52:32 +0900

    fix: align manifest workflow and command schema metadata

--- src/types/command.ts
+++ src/types/command.ts
@@ -2,7 +2,7 @@ import { Type as T } from "@sinclair/typebox";
 import { StaticDecode } from "@sinclair/typebox";
 
 export const commandSchema = T.Object({
-  name: T.Literal("query"),
+  name: T.Literal("query", { description: "Returns the user's wallet, access, and multiplier information.", examples: ['/query @UbiquityOS'] }),
   parameters: T.Object({
     username: T.String(),
   }),
